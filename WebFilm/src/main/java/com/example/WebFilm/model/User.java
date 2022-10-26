6498a11c5594ab3fcd43c74a9c8809e576b0c49a
VanKhai <vokhaipm090@gmail.com>
2022-10-26 23:54:35 +0700

    Up spring

---
+++ WebFilm/src/main/java/com/example/WebFilm/model/User.java
@@ -0,0 +1,53 @@
+package com.example.WebFilm.model;
+
+import java.util.Date;
+
+public class User {
+	
+	private long id;
+	private String fullName;
+	private String userName;
+	private String userPassword;
+	private String email;
+	private String phone;
+	
+
+		public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+		
+}
